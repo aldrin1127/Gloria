8554b3bbdbde553dadc72606f76943dc5fd89c61
aldrinbalitbit <aldrinbalitbit1127@gmail.com>
2024-12-23 13:33:09 +0800

    Added shared folder

---
+++ shared/build.gradle.kts
@@ -0,0 +1,66 @@
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.jetbrains.compose)
+    alias(libs.plugins.compose.compiler)
+}
+
+kotlin {
+    targetHierarchy.default()
+    androidTarget {
+        compilations.all {
+            kotlinOptions.jvmTarget = libs.versions.jvm.get()
+        }
+    }
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "shared"
+            isStatic = true
+        }
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                api(libs.kotlin.bom)
+                api(libs.kotlin.stdlib)
+                api(compose.runtime)
+                api(compose.foundation)
+                api(compose.material3)
+
+                api(compose.ui)
+                api(compose.components.resources)
+            }
+        }
+        val commonTest by getting {
+            dependsOn(commonMain)
+            dependencies {
+                api(libs.kotlin.test)
+                api(libs.kotlin.test.junit5)
+            }
+        }
+    }
+}
+
+android {
+    namespace  = "${Artifact.APP_ID}"
+    compileSdk = libs.versions.android.compile.sdk.get().toInt()
+    defaultConfig {
+        minSdk = libs.versions.android.min.sdk.get().toInt()
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.toVersion(libs.versions.jvm.get())
+        targetCompatibility = JavaVersion.toVersion(libs.versions.jvm.get())
+    }
+}
+
+compose.resources {
+    publicResClass    = true
+    packageOfResClass = "${Artifact.APP_ID}.res"
+    generateResClass  = always
+}
